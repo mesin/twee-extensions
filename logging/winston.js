49d3ba3072f1356c29fb028de6c16d0b3be48e20
Dmitri Mesin <dmitriym@elance-odesk.com>
2015-01-07 23:59:43 +0200

    Using also console logging to see details of requests

--- logging/winston.js
+++ logging/winston.js
@@ -11,6 +11,16 @@ module.exports.extension = function() {
         transports: [
             new winston.transports.File({
                 filename: path.join(twee.getBaseDirectory(), twee.getConfig('twee:options:logging:winston:accessFile'))
+            }),
+            new winston.transports.Console({
+                colorize: true,
+                // optional: control whether you want to log the meta data about the request (default to true)
+                meta: true,
+                msg: "[TWEE] HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}",
+                // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true.
+                // Will only output colors on transports with colorize set to true
+                expressFormat: true,
+                colorStatus: true
             })
         ],
         exceptionHandlers: [
